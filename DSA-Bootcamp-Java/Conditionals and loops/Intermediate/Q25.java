01fa6d5302ae29f45d07c664c6b6f76abc10b367
Abhikalp Arya <en20cs303003@medicaps.ac.in>
2022-02-02 21:48:11 +0530

    Add files via upload

---
+++ DSA-Bootcamp-Java/Conditionals and loops/Intermediate/Q25.java
@@ -0,0 +1,13 @@
+package com.abhikalp;
+
+public class Q25 {
+    public static void main(String[] args) {
+        int count =0;
+        for (int i=1; i <= 31; i++) {
+            if (i % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println("No. of days = " + count);
+    }
+}
